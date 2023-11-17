328fa492d8bd1d7137047bc26e2c55435c936f82
Administrator <admin@example.com>
2023-11-17 22:31:08 +0330

    dev discovery server

--- tax_pay_sys/src/main/java/com/cyancoder/tax_pay_sys_service/TaxPaySysServiceApplication.java
+++ tax_pay_sys/src/main/java/com/cyancoder/tax_pay_sys_service/TaxPaySysServiceApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
-@EnableEurekaClient
 public class TaxPaySysServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaxPaySysServiceApplication.class, args);

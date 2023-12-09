9cbf94754863f6c721ae5d49d57ab18d9b22d0bc
Administrator <admin@example.com>
2023-12-09 13:32:30 +0330

    dev factor service

--- factor-service/src/main/java/com/cyancoder/factor/FactorServiceApplication.java
+++ factor-service/src/main/java/com/cyancoder/factor/FactorServiceApplication.java
@@ -1,4 +1,10 @@
 package com.cyancoder.factor;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FactorServiceApplication {
+    public static void main(String[] args) {
+
+    }
 }

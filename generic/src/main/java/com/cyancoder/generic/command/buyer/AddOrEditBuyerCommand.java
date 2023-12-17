a7fc9079a3d4332c2ed18d94c05e59755938a4b5
Administrator <admin@example.com>
2023-12-17 21:46:45 +0330

    dev saga

---
+++ generic/src/main/java/com/cyancoder/generic/command/buyer/AddOrEditBuyerCommand.java
@@ -0,0 +1,17 @@
+package com.cyancoder.generic.command.buyer;
+
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class AddOrEditBuyerCommand {
+
+
+    @TargetAggregateIdentifier
+    private final Long buyerId;
+
+
+}

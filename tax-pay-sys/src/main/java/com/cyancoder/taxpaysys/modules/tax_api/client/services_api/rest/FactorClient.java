9cf44ba7412850571041e63fbfec6fc854a7544c
farid-cyan <info@cyancoder.co>
2024-01-01 21:08:43 +0330

    some changes

--- tax-pay-sys/src/main/java/com/cyancoder/taxpaysys/modules/tax_api/client/services_api/rest/FactorClient.java
+++ tax-pay-sys/src/main/java/com/cyancoder/taxpaysys/modules/tax_api/client/services_api/rest/FactorClient.java
@@ -4,11 +4,11 @@ package com.cyancoder.taxpaysys.modules.tax_api.client.services_api.rest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "factor-service",url = "lb://factor-service/v2/api/factor-service")////////// need to consider :lb
+@FeignClient(name = "factor-service")////////// need to consider :lb
 public interface FactorClient {
 
 
-    @GetMapping(value = "/factors")
+    @GetMapping(value = "/v2/api/factor-service/factors")
     public Object getItems();
 //    public List<AbstractReadWriteAccess.Item> getItems(@RequestParam Long userId);
 }

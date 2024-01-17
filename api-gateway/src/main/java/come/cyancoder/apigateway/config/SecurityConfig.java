7669c93480e10342d52e57912b3cb7053c242b4b
Administrator <admin@example.com>
2024-01-17 18:36:17 +0330

    dev CORS gateway-service

--- api-gateway/src/main/java/come/cyancoder/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/come/cyancoder/apigateway/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package come.cyancoder.apigateway.config;
 
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,19 +19,33 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity
+    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity, GlobalCorsProperties corsProperties){
+        serverHttpSecurity		.cors(cors -> cors.disable())
                 .csrf()
                 .disable()
+                .cors(corsSpec -> corsSpec.configurationSource(unused -> corsProperties.getCorsConfigurations().get("/**")))
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**")
                         .permitAll()
                         .pathMatchers("/realms/cyan/protocol/openid-connect/**")
                         .permitAll()
                         .anyExchange()
-                        .authenticated())
+                        .authenticated()
+                )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
 
     }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }

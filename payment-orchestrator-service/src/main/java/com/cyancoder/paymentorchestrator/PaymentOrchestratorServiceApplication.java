2d44e026cdd0c01f281aeeee207ade54cec4f571
Farid Mg <faridmg9271@gmail.com>
2026-06-07 17:04:22 +0330

    fix bug

--- payment-orchestrator-service/src/main/java/com/cyancoder/paymentorchestrator/PaymentOrchestratorServiceApplication.java
+++ payment-orchestrator-service/src/main/java/com/cyancoder/paymentorchestrator/PaymentOrchestratorServiceApplication.java
@@ -5,7 +5,7 @@ import com.cyancoder.dynamiccore.config.DynamicPersistenceConfig;
 import com.cyancoder.dynamiccore.security.DualApiSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
